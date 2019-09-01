0b368f74d6a5795ac2f96bb7d1762ae3c9bd74d4
MARIYA ELIYASEVYCH <meliyasevych@gmail.com>
2019-09-01 13:56:00 -0500

    first product

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,9 @@
 class Api::ProductsController < ApplicationController
+  def all_products
+    @products = Product.all
+    render 'all_products.json.jb'
+  end
+  def first_product 
+    @product = Product.first
+    render 'first_product.json.jb'
 end
